e4e2e73ab48430efc06ecd36195c4ea611708a7b
Ashrya <ashryaagr@gmail.com>
2019-01-01 15:55:22 +0530

    blog finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,52 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :update, :edit, :destroy]
 
-def index
-    @posts = Post.all.order("created_at DESC")
-end
 
-def new
-    @post = Post.new
-end
-
-def create
-    @post = Post.new(post_params)
-
-    if @post.save
-        redirect_to @post
-    else 
-        render 'new'
+    def index
+        @posts = Post.all.order("created_at DESC")
     end
-end
 
-def update
-    @post = Post.find(params[:id])
-
-    if @post.update(post_params)
-        redirect_to @post        
-    else
-        render 'edit'
+    def new
+        @post = Post.new
     end
-end
 
-def edit
-    @post = Post.find(params[:id])
-end
+    def create
+        @post = Post.new(post_params)
 
-def show
-    @post = Post.find(params[:id])
-end
+        if @post.save
+            redirect_to @post
+        else 
+            render 'new'
+        end
+    end
 
-def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+    def update
+        if @post.update(post_params)
+            redirect_to @post        
+        else
+            render 'edit'
+        end
+    end
 
-    redirect_to posts_path
-end
+    def edit
+    end
 
-private
+    def show
+    end
 
-def post_params
-    params.require(:post).permit(:title, :content)
-end
+    def destroy
+        @post.destroy
 
+        redirect_to posts_path
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+       @post = Post.find(params[:id]) 
+    end
 end
